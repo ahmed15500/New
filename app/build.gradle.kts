6a485d6c37d0f0385fa93847d67804e69806796a
ahmed15500 <34009779+ahmed15500@users.noreply.github.com>
2026-07-11 15:58:38 +0300

    Release Midani version 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.ahmed.yawmeyaty"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "2.0.0"
 
         vectorDrawables {
             useSupportLibrary = true
